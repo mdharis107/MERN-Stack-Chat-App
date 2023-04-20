cae4ff086e5a31e49288aecf7b8912f28b7dbadb
HARIS#10# <mdharishz107@gmail.com>
2023-04-21 00:34:17 +0530

    added frontend basics

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const { chats } = require("./data/data");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send("this is the Home page")
-})
+const PORT = process.env.PORT || 8000;
 
-app.listen(8080,console.log("here"))
+app.get("/", (req, res) => {
+  res.send("this is the Home page");
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, console.log(`Server is listening on PORT ${PORT}`));
