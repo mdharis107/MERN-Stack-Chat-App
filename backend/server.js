d0c38b5b4132bc9f53fc12233c2bf5e4c45ca001
HARIS#10# <mdharishz107@gmail.com>
2023-04-21 00:40:19 +0530

    added frontend basics

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.send("this is the Home page");
 });
 
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
 app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
