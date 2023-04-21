5a0cf0360c8e9edc94f7e2d27a52a33b5aa7baff
HARIS#10# <mdharishz107@gmail.com>
2023-04-21 16:10:50 +0530

    created chat Model inn backend

---
+++ backend/models/chat.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    user: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const chatModel = mongoose.model("chats", chatSchema);
+
+module.exports = {
+  chatModel,
+};
