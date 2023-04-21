a71e8260dabf7544f9a9c6714044f3a181a838cf
HARIS#10# <mdharishz107@gmail.com>
2023-04-21 15:24:33 +0530

    updated frontend

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const ChatPage = () => {
-  return (
-    <div>ChatPage</div>
-  )
-}
+  const [chats, setChats] = useState([]);
+  const fetchChats = async () => {
+    const  data  = await axios.get("http://localhost:8080/api/chat");
+    // console.log(data);
+    setChats(data.data);
+  };
 
-export default ChatPage
+  useEffect(() => {
+    fetchChats();
+  }, []);
+  return (
+    <div>
+      {chats.map((ele) => (
+        <div key={ele._id} >
+          <h3>{ele.chatName}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ChatPage;
