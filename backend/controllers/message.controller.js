011399df8232e0de7aa90a784bc4ed49886e7f24
HARIS#10# <mdharishz107@gmail.com>
2023-05-24 18:28:30 +0530

    updated the socket.io in the backend

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -4,28 +4,58 @@ const { UserModel } = require("../models/user.model");
 
 const sendMessage = async (req, res) => {
   const { content, chatId } = req.body;
+
   if (!content || !chatId) {
     console.log("Invalid data passed into request");
     res.status(400).send("Invalid Data");
   }
 
-  var newMessage = {
+  // var newMessage = {
+  //   sender: req.user._id,
+  //   content: content,
+  //   chat: chatId,
+  // };
+
+  const newMessage = new MessageModel({
     sender: req.user._id,
     content: content,
     chat: chatId,
-  };
+  });
 
   try {
-    var message = await MessageModel.create(newMessage);
-
+    const message = await newMessage.save();
     await message.populate("sender", "name pic");
+    await message.populate("chat");
 
-    // await message.populate("chat");
+    // var message = await MessageModel.create(newMessage);
 
-    await UserModel.populate(message, {
+    // await message
+    //   .populate({ path: "sender", select: "name pic" })
+    //   .populate("chat");
+
+    // await message.populate({ path: "chat", select: "isGroupChat chatName" });
+
+    // const data = await message.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: message,
+    //       localField: "_id",
+    //       foreignField: "_id",
+    //       as: "chat",
+    //     },
+    //   },
+    // ]);
+
+    // console.log(data);
+
+    // console.log(message);
+
+     await UserModel.populate(message, {
       path: "chat.users",
       select: "name pic email",
     });
+
+    // console.log(message, "here");
 
     await ChatModel.findByIdAndUpdate(req.body.chatId, {
       latestMessage: message,
@@ -44,7 +74,7 @@ const allMessages = async (req, res) => {
     const messages = await MessageModel.find({
       chat: req.params.chatId,
     }).populate("sender", "name pic email");
-    // .populate("chat");
+    // .populate("users");
 
     res.send(messages);
   } catch (err) {
